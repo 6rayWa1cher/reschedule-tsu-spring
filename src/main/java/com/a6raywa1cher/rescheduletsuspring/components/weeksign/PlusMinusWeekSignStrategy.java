111931600a9284b567cd9bac2f9ecc6df75b07c2
6rayWa1cher <27364356+6rayWa1cher@users.noreply.github.com>
2019-09-08 22:30:42 +0300

    fix, attempt 2

--- src/main/java/com/a6raywa1cher/rescheduletsuspring/components/weeksign/PlusMinusWeekSignStrategy.java
+++ src/main/java/com/a6raywa1cher/rescheduletsuspring/components/weeksign/PlusMinusWeekSignStrategy.java
@@ -6,14 +6,14 @@ import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.TimeZone;
+import java.util.Locale;
 
 public class PlusMinusWeekSignStrategy implements WeekSignStrategy {
 	@Override
 	public WeekSign getWeekSign(Date date, LocalDate startDate, WeekSign startWeekSign) {
-		Calendar current = Calendar.getInstance(TimeZone.getTimeZone("Europe/Moscow"));
+		Calendar current = Calendar.getInstance(Locale.forLanguageTag("ru-RU"));
 		current.setTime(date);
-		Calendar start = Calendar.getInstance();
+		Calendar start = Calendar.getInstance(Locale.forLanguageTag("ru-RU"));
 		start.setTime(Date.from(startDate.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant()));
 		int startWeek = start.get(Calendar.WEEK_OF_YEAR);
 		int currWeek = current.get(Calendar.WEEK_OF_YEAR);
